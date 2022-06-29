de654d6b13915b37a0959c1007d0e93ae5490d8f
effiRin <effirin8@gmail.com>
2022-06-30 01:01:28 +0900

    <Plus One> in LeetCode (0630)

--- com/yerin/LeetCode/ArrayAndString/PlusOne.java
+++ com/yerin/LeetCode/ArrayAndString/PlusOne.java
@@ -3,34 +3,37 @@ package com.yerin.LeetCode.ArrayAndString;
 import java.util.Arrays;
 
 public class PlusOne {
+//    https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1148/
 
     public int[] plusOne(int[] digits) {
 
-        String str = null;
-        int num = 0;
-        String plusOneNum = null;
+        String str = "";
+        long num = 0;
+        String plusOneNum = "";
+
 
         for (int i = 0; i < digits.length; i++) {
             str += Integer.toString(digits[i]);
-
         }
 
-        num = Integer.parseInt(str) + 1;
+        System.out.println(str);
 
-        plusOneNum = Integer.toString(num);
-        int[] arr = new int[plusOneNum.length()];
+        num = Long.parseLong(str) + 1;
+
+        plusOneNum = Long.toString(num);
+        int[] plusOneArr = new int[plusOneNum.length()];
 
         for (int i = 0; i < plusOneNum.length(); i++) {
-            arr[i] = plusOneNum.charAt(i)-'0';
+            plusOneArr[i] = plusOneNum.charAt(i)-'0';
         }
 
-        return arr;
+        return plusOneArr;
     }
 
     public static void main(String[] args) {
         int[] digits1 = {1, 2, 3};
         int[] digits2 = {4, 3, 2, 1};
-        int[] digits3 = {9};
+        int[] digits3 = {9,8,7,6,5,4,3,2,1,0};
 
         PlusOne s = new PlusOne();
         System.out.println(Arrays.toString(s.plusOne(digits1)));
