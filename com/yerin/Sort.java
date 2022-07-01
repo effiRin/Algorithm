a6801430854bd62545f83267805f5dddec6bef76
effiRin <effirin8@gmail.com>
2022-07-02 00:02:30 +0900

    Coding Test - Sort (0701)

---
+++ com/yerin/Sort.java
@@ -0,0 +1,30 @@
+package com.yerin;
+
+public class Sort {
+
+    public static void main(String[] args) {
+
+        Integer[] array = new Integer[]{4, 9, 5, 6, 7, 8, 1, 2, 3, 0};
+
+        Integer[] temp = new Integer[array.length];
+
+        int min = 4;
+        int max = 9;
+
+        // Sorting
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+
+            }
+
+        }
+
+
+        // Show result
+        for (int i = 0; i < array.length; ++i) {
+            System.out.println(array[i]);
+        } // for
+
+    } // main
+
+}// class
