f4d597d85058e0322c4ebbc730377b90a555590e
effiRin <effirin8@gmail.com>
2022-07-03 23:56:53 +0900

    <Diagonal Traverse> in LeetCode (0703)

---
+++ com/yerin/LeetCode/DiagonalTraverse.java
@@ -0,0 +1,27 @@
+package com.yerin.LeetCode;
+
+public class DiagonalTraverse {
+    // https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1167/
+
+    public int[] findDiagonalOrder(int[][] mat) {
+
+        for (int i = 0; i < mat.length; i++) {
+            int m = mat.length;
+            int n = mat[i].length;
+
+        }
+
+        int[] result = new int[mat.length];
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] mat1 = {{1,2,3},{4,5,6},{7,8,9}};       // [1,2,4,7,5,3,6,8,9]
+        int[][] mat2 = {{1,2},{3,4}};      // [1,2,3,4]
+
+
+
+    }
+}
