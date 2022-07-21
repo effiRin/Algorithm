fb29d2cb3a6b41baeffa0d440ab03b6e914b0cf9
effiRin <effirin8@gmail.com>
2022-07-21 15:21:18 +0900

    Q3.<Sum Of Section>(220720)_feedback

--- com/yerin/BaekJoon/DoIt/SumOfSection.java
+++ com/yerin/BaekJoon/DoIt/SumOfSection.java
@@ -3,44 +3,48 @@ package com.yerin.BaekJoon.DoIt;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class SumOfSection {
+
+    // https://www.acmicpc.net/problem/11659
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String[] str = br.readLine().split(" ");
-        int n = Integer.parseInt(str[0]);
-        int m = Integer.parseInt(str[1]);
+        int n = Integer.parseInt(st.nextToken());  // 데이터의 개수
+        int m = Integer.parseInt(st.nextToken());  // 물어볼 개수
 
-        int[] nums = new int[n];
-        int[] sum = new int[n];
+//        int[] nums = new int[n];    // 사실상 필요없음 -> 합 배열만 구하면 됨
+        long[] sum = new long[n + 1]; // 인덱스 1부터 쓰기 위해서
 
-        String[] temp = br.readLine().split(" ");
+        st = new StringTokenizer(br.readLine());    // 새로운 인스턴스 만들어서 두 번째 줄 입력 받음
 
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(temp[i]);
+        // 인덱스 0부터 쓰려면 이렇게 해야 함...
+//        for (int i = 0; i < n; i++) {
+//            nums[i] = Integer.parseInt(st.nextToken());
+//
+//            if(i != 0){sum[i] = sum[i - 1] + nums[i];}
+//            else {sum[i] = nums[i];}
+//        } // for
 
-            if(i != 0){sum[i] = sum[i - 1] + nums[i];}
-            else {sum[i] = nums[i];}
-        } // for
+        // 인덱스 1부터 - sum[0]은 기본값 0으로 들어감
+        System.out.println(sum[0]);
+
+        for (int i = 1; i < n+1; i++) {
+            sum[i] = sum[i-1] + Integer.parseInt(st.nextToken());
+        }
 
         for(int a = 0; a < m; a++) {
-            String[] section = br.readLine().split(" ");
+            st = new StringTokenizer(br.readLine());   //
 
-            int i  = Integer.parseInt(section[1])-1;
-            int j  = Integer.parseInt(section[0])-2;
+            int i  = Integer.parseInt(st.nextToken());
+            int j  = Integer.parseInt(st.nextToken());
 
-            if (i != j && j >= 0){
-                System.out.println(sum[i]-sum[j]);
-            }
-            else if(j == -1){
-                System.out.println(sum[i]);
-            }
-            else {
-                System.out.println(nums[i]);
-            }
+            System.out.println(sum[j] - sum[i - 1]);
+
         } // for
 
     }// main
